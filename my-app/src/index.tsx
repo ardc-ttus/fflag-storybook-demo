477b33e5ea2e583f4276d0e0f6520c0ed4c19d3a
Rodolpho Alves <83079144+ardc-ttus@users.noreply.github.com>
2023-05-17 02:00:58 +0000

    chore: init Modus

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "@trimbleinc/modus-react-bootstrap/css/dist/modus-react-bootstrap.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
